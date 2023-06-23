050a3c52915caa22cf007c0e7baaefd5d0b7f940
roodhouse <rughjm@gmail.com>
2023-06-23 17:17:20 -0500

    background adjustment

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
     <div className="App">
       
       <div id='container' className='h-screen'>
-        <div id='backgroundWrapper' className='absolute w-full'>
-          <div id='backgroundImg' className='bg-[url("./assets/mobile/image-host.jpg")] h-screen'></div>
-          <div id='background' className='h-screen opacity-[0.7992919683456421] bg-darkBlue absolute w-full bottom-0'></div>
+        <div id='backgroundWrapper' className='absolute w-full md:flex md:justify-end'>
+          <div id='backgroundImg' className='bg-[url("./assets/mobile/image-host.jpg")] md:bg-[url("./assets/tablet/image-host.jpg")] h-screen md:h-[767px] md:w-[491px] md:absolute md:bg-no-repeat md:z-10'></div>
+          <div id='background' className='h-screen opacity-[0.7992919683456421] bg-darkBlue absolute w-full bottom-0 md:hidden'></div>
         </div>
-        <div id='wrapper' className='h-full pt-[62px] px-6 pb-[94px] relative'>
-          <div id='header' className='flex justify-center mb-[57px]'>
+        <div id='wrapper' className='h-full pt-[62px] px-6 pb-[94px] relative md:pt-[50px] md:pl-[30px] md:pr-[94px] md:bg-darkBlue'>
+          <div id='header' className='flex justify-center mb-[57px] md:justify-start'>
             <Header />
           </div>
           <div id='body' className='mb-[36px]'>
