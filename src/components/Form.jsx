49b310a551e1c345a5cdbe1e1e19802a0fcc2a5e
roodhouse <rughjm@gmail.com>
2023-06-24 11:14:23 -0500

    hover color

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -52,7 +52,6 @@ function Form() {
         <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col justify-center items-center md:flex-row md:flex-wrap'>
             <div id='emailDiv' className='mb-4 w-full md:mb-0 md:w-[50%]'>
                 <input 
-                    
                     title='Please match the format I asked you to'
                     id='email' 
                     type="email" 
@@ -71,7 +70,7 @@ function Form() {
             </div>
             
             <div id='submitDiv' className='w-full md:bg-medBlue md:rounded-[0px_28px_28px_0px] md:py-[5px] md:pr-[5px] md:w-[50%]'>
-                <input name='submit' type="submit" id="submit" value='Request Access' className='w-full rounded-[28px] bg-green shadow-[0_25px_20px_-20px_rgba(84,230,175,0.50)] text-darkBlue text-sm leading-[28px] font-["Chivo"] py-[9px]' />                    
+                <input name='submit' type="submit" id="submit" value='Request Access' className='w-full rounded-[28px] bg-green shadow-[0_25px_20px_-20px_rgba(84,230,175,0.50)] text-darkBlue text-sm leading-[28px] font-["Chivo"] py-[9px] cursor-pointer hover:bg-lightGreen' />                    
             </div>
             <p id='errorP' className='text-warning text-sm font-["Chivo"] pl-8 mt-2'></p>
             {errors.email?.message && (
